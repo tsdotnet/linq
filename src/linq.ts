a96176265e311ac8199a02bcf3c7321797382e2f
ThomasBarnekow <thomas@barnekow.info>
2022-01-30 14:01:13 +0100

    Fix pattern

--- src/linq.ts
+++ src/linq.ts
@@ -2,76 +2,112 @@
  * @author electricessence / https://github.com/electricessence/
  * @license MIT
  */
-import LinqBase from './LinqBase';
-import { IterableFilter, IterableValueTransform } from './IterableTransform';
+
+import { PredicateWithIndex, SelectorWithIndex } from '@tsdotnet/common-interfaces';
+import { IterableFilter, IterableTransform, IterableValueTransform } from './IterableTransform';
+import where from './filters/where';
+import applyFilters from './applyFilters';
 import select from './transforms/select';
 import selectMany from './transforms/selectMany';
-import { SelectorWithIndex } from '@tsdotnet/common-interfaces';
+import skip from './filters/skip';
+import take from './filters/take';
+
+export abstract class LinqBase<T, TLinq extends LinqBase<T, TLinq>> implements Iterable<T> {
+  constructor(protected readonly source: Iterable<T>, protected readonly create: (source: Iterable<T>) => TLinq) {}
+
+  [Symbol.iterator](): Iterator<T> {
+    return this.source[Symbol.iterator]();
+  }
+
+  /**
+   * Returns a filtered sequence.
+   * @param {IterableFilter<T>} filters The filters to use.
+   * @return {TLinq<T>}
+   */
+  filter(...filters: IterableFilter<T>[]): TLinq {
+    return this.applyFilters(filters);
+  }
+
+  /**
+   * Returns a filtered sequence.
+   * @param {IterableFilter<T>} filters The filters to use.
+   * @return {TLinq<T>}
+   */
+  applyFilters(filters: Iterable<IterableFilter<T>>): TLinq {
+    return this.create(applyFilters(this.source, filters));
+  }
+
+  /**
+   * Returns a transformed sequence.
+   * @param {IterableValueTransform<T, TResult>} transform The transform to use.
+   * @return {Iterable<TResult>}
+   */
+  transform<TResult>(transform: IterableValueTransform<T, TResult>): Linq<TResult> {
+    return linq(transform(this.source));
+  }
+
+  /**
+   * Applies a resolver to this sequence.
+   * @param {IterableTransform<T, TResolution>} resolver
+   * @return {TResolution}
+   */
+  resolve<TResolution>(resolver: IterableTransform<T, TResolution>): TResolution {
+    return resolver(this.source);
+  }
+
+  /**
+   * Filters a sequence of values based on a predicate.
+   * @param {PredicateWithIndex<T>} predicate
+   * @return {TLinq<T>}
+   */
+  where(predicate: PredicateWithIndex<T>): TLinq {
+    return this.filter(where(predicate));
+  }
+
+  /**
+   * When resolving, skips the number of elements by the count.
+   * @param {number} count The number elements to skip.
+   * @return {LinqExtended<T>}
+   */
+  skip(count: number): TLinq {
+    return this.filter(skip<T>(count));
+  }
+
+  /**
+   * When resolving, takes no more than the number of elements by the provided count.
+   * @param {number} count The number elements to skip.
+   * @return {LinqExtended<T>}
+   */
+  take(count: number): TLinq {
+    return this.filter(take(count));
+  }
+
+  /**
+   * Projects each element of a sequence into a new form.
+   * @param {SelectorWithIndex<T, TResult>} selector
+   * @return {Iterable<TResult>}
+   */
+  select<TResult>(selector: SelectorWithIndex<T, TResult>): Linq<TResult> {
+    return linq(select(selector)(this.source));
+  }
+
+  /**
+   * Projects each element of iterables as a flattened sequence of the selected.
+   * @param {SelectorWithIndex<T, Iterable<TResult>>} selector
+   * @return {Iterable<TResult>}
+   */
+  selectMany<TResult>(selector: SelectorWithIndex<T, Iterable<TResult>>): Linq<TResult> {
+    return linq(selectMany(selector)(this.source));
+  }
+}
 
 /**
  * Simplest abstraction for building an extensible iterable query.
  */
-export class Linq<T> extends LinqBase<T, Linq<T>>
-{
-	constructor(
-		protected readonly source: Iterable<T>) {
-		super(source, source => new Linq(source));
-	}
-
-	[Symbol.iterator](): Iterator<T> {
-		return this.source[Symbol.iterator]();
-	}
-
-	/**
-	 * Returns a filtered sequence.
-	 * Same effect as .transform(filter).
-	 * @param {IterableValueTransform<T, TResult>} filter
-	 * @return {Linq<TResult>}
-	 */
-	filter<TResult>(filter: IterableValueTransform<T, TResult>): Linq<TResult>;
-
-	/**
-	 * Returns a filtered sequence.
-	 * @param {IterableFilter<T>} filters The filters to use.
-	 * @return {Linq<T>}
-	 */
-	filter(filter: IterableFilter<T>): Linq<T>;
-
-	/**
-	 * Returns a filtered sequence.
-	 * @param {IterableFilter<T>} filters The filters to use.
-	 * @return {TLinq<T>}
-	 */
-	filter(filter: IterableFilter<T>): Linq<T> {
-		return super.filter(filter);
-	}
-
-	/**
-	 * Returns a transformed sequence.
-	 * @param {IterableValueTransform<T, TResult>} transform The transform to use.
-	 * @return {Linq<TResult>}
-	 */
-	transform<TResult>(transform: IterableValueTransform<T, TResult>): Linq<TResult> {
-		return new Linq(transform(this.source));
-	}
-
-	/**
-	 * Projects each element of a sequence into a new form.
-	 * @param {SelectorWithIndex<T, TResult>} selector
-	 * @return {Linq<TResult>}
-	 */
-	select<TResult>(selector: SelectorWithIndex<T, TResult>): Linq<TResult> {
-		return this.transform(select(selector));
-	}
-
-	/**
-	 * Projects each element of iterables as a flattened sequence of the selected.
-	 * @param {SelectorWithIndex<T, Iterable<TResult>>} selector
-	 * @return {Linq<TResult>}
-	 */
-	selectMany<TResult>(selector: SelectorWithIndex<T, Iterable<TResult>>): Linq<TResult> {
-		return this.transform(selectMany(selector));
-	}
+export class Linq<T> extends LinqBase<T, Linq<T>> {
+  constructor(source: Iterable<T>) {
+    super(source, linq);
+  }
 }
 
 /**
@@ -82,6 +118,6 @@ export class Linq<T> extends LinqBase<T, Linq<T>>
  * @return {Linq<T>}
  */
 export default function linq<T>(source: Iterable<T>): Linq<T> {
-	if (source instanceof Linq) return source;
-	return new Linq<T>(source);
+  if (source instanceof Linq) return source;
+  return new Linq<T>(source);
 }
--- src/linqExtended.ts
+++ src/linqExtended.ts
@@ -3,98 +3,203 @@
  * @license MIT
  */
 
-import { SelectorWithIndex } from '@tsdotnet/common-interfaces';
-import { IterableFilter, IterableValueTransform } from './IterableTransform';
+import { PredicateWithIndex, SelectorWithIndex } from '@tsdotnet/common-interfaces';
+import { IterableValueTransform } from './IterableTransform';
+import { LinqBase } from './linq';
 import groupBy, { Grouping, GroupingResult } from './transforms/groupBy';
 import select from './transforms/select';
 import selectMany from './transforms/selectMany';
-import LinqResolverBase from './LinqResolverBase';
+import defaultIfEmpty from './filters/defaultIfEmpty';
+import where from './filters/where';
+import all from './resolutions/all';
+import any from './resolutions/any';
+import count from './resolutions/count';
+import toArray from './resolutions/toArray';
+import single from './resolutions/single';
+import singleOrDefault from './resolutions/singleOrDefault';
+import first from './resolutions/first';
+import firstOrDefault from './resolutions/firstOrDefault';
+import last from './resolutions/last';
+import lastOrDefault from './resolutions/lastOrDefault';
+
+export abstract class LinqExtendedBase<T, TLinq extends LinqExtendedBase<T, TLinq>> extends LinqBase<T, TLinq> {
+  /**
+   * Returns a transformed sequence.
+   * @param {IterableValueTransform<T, TResult>} transform The transform to use.
+   * @return {LinqExtended<TResult>}
+   */
+  override transform<TResult>(transform: IterableValueTransform<T, TResult>): LinqExtended<TResult> {
+    return linqExtended(transform(this.source));
+  }
+
+  /**
+   * Projects each element of a sequence into a new form.
+   * @param {SelectorWithIndex<T, TResult>} selector
+   * @return {LinqExtended<TResult>}
+   */
+  override select<TResult>(selector: SelectorWithIndex<T, TResult>): LinqExtended<TResult> {
+    return this.transform(select(selector));
+  }
+
+  /**
+   * Projects each element of iterables as a flattened sequence of the selected.
+   * @param {SelectorWithIndex<T, Iterable<TResult>>} selector
+   * @return {LinqExtended<TResult>}
+   */
+  override selectMany<TResult>(selector: SelectorWithIndex<T, Iterable<TResult>>): LinqExtended<TResult> {
+    return this.transform(selectMany(selector));
+  }
+
+  /**
+   * Groups entries together by selected key.
+   * @param {SelectorWithIndex<T, TKey>} keySelector
+   * @return {LinqExtended<Grouping<TKey, T>>}
+   */
+  groupBy<TKey>(keySelector: SelectorWithIndex<T, TKey>): LinqExtended<LinqGrouping<TKey, T>> {
+    return this.transform(groupBy(keySelector)).select((g) => new LinqGrouping<TKey, T>(g));
+  }
+
+  /**
+   * Returns the elements of the specified sequence or the default value if the
+   * sequence is empty.
+   *
+   * @param defaultValue The default value to be returned if this sequence is empty.
+   * @returns The elements of the specified sequence or the default value if the sequence is empty.
+   */
+  defaultIfEmpty(defaultValue: T): TLinq {
+    return this.create(defaultIfEmpty(defaultValue)(this.source));
+  }
+
+  /**
+   * Returns all the entries in the sequence as a new array.
+   * @return {T[]}
+   */
+  toArray(): T[] {
+    return toArray(this.source);
+  }
+
+  /**
+   * Returns the number of entries in a sequence.
+   * If a predicate is provided, filters the count based upon the predicate.
+   * Otherwise counts all the entries in the sequence.
+   * @param {PredicateWithIndex<T>} predicate
+   * @return {boolean}
+   */
+  count(predicate?: PredicateWithIndex<T>): number {
+    return predicate ? count(where(predicate)(this.source)) : count(this.source);
+  }
+
+  /**
+   * Returns true if the predicate ever returns true. Otherwise false.
+   * If no predicate is provided, returns true if the sequence has any entries.
+   * @param {PredicateWithIndex<T>} predicate
+   * @return {boolean}
+   */
+  any(predicate?: PredicateWithIndex<T>): boolean {
+    return any(predicate)(this.source);
+  }
+
+  /**
+   * Returns false if the predicate ever returns false. Otherwise true.
+   * @param {PredicateWithIndex<T>} predicate
+   * @return {boolean}
+   */
+  all(predicate: PredicateWithIndex<T>): boolean {
+    return all(predicate)(this.source);
+  }
+
+  /**
+   * Returns the single element of the sequence.
+   * If a predicate is provided, filters the sequence based on the predicate.
+   *
+   * @param predicate The optional predicate.
+   * @returns The single element.
+   * @throws If the (optionally filtered) sequence does not contain exactly one element.
+   */
+  single(predicate?: PredicateWithIndex<T>): T {
+    return single(this.filterSequence(predicate));
+  }
+
+  /**
+   * Returns the single element of the sequence or `null`, if the sequence is empty.
+   * If a predicate is provided, filters the sequence based on the predicate.
+   *
+   * @param predicate The optional predicate.
+   * @returns The single element or `null`.
+   * @throws If the (optionally filtered) sequence contains more than one element.
+   */
+  singleOrDefault(predicate?: PredicateWithIndex<T>): T | null {
+    return singleOrDefault<T>()(this.filterSequence(predicate)) ?? null;
+  }
+
+  /**
+   * Returns the first element of the sequence.
+   * If a predicate is provided, filters the sequence based on the predicate.
+   *
+   * @param predicate The optional predicate.
+   * @returns The first element.
+   * @throws If the (optionally filtered) sequence is empty.
+   */
+  first(predicate?: PredicateWithIndex<T>): T {
+    return first(this.filterSequence(predicate));
+  }
+
+  /**
+   * Returns the first element of the sequence or `null`, if the sequence is empty.
+   * If a predicate is provided, filters the sequence based on the predicate.
+   *
+   * @param predicate The optional predicate.
+   * @returns The first element or `null`.
+   */
+  firstOrDefault(predicate?: PredicateWithIndex<T>): T | null {
+    return firstOrDefault<T>()(this.filterSequence(predicate)) ?? null;
+  }
+
+  /**
+   * Returns the last element of the sequence.
+   * If a predicate is provided, filters the sequence based on the predicate.
+   *
+   * @param predicate The optional predicate.
+   * @returns The last element.
+   * @throws If the (optionally filtered) sequence is empty.
+   */
+  last(predicate?: PredicateWithIndex<T>): T {
+    return last(this.filterSequence(predicate));
+  }
+
+  /**
+   * Returns the last element of the sequence or `null`, if the sequence is empty.
+   * If a predicate is provided, filters the sequence based on the predicate.
+   *
+   * @param predicate The optional predicate.
+   * @returns The last element or `null`.
+   */
+  lastOrDefault(predicate?: PredicateWithIndex<T>): T | null {
+    return lastOrDefault<T>()(this.filterSequence(predicate)) ?? null;
+  }
+
+  /** @internal */
+  private filterSequence(predicate?: PredicateWithIndex<T>): Iterable<T> {
+    return predicate ? where(predicate)(this.source) : this.source;
+  }
+}
 
 /**
  * Extended version of `Linq<T>` that includes common LINQ methods like `.where()` and `.select()` and `.groupBy()`.
  */
-export class LinqExtended<T>
-	extends LinqResolverBase<T, LinqExtended<T>>
-{
-	constructor(
-		protected readonly source: Iterable<T>) {
-		super(source, source => new LinqExtended(source));
-	}
-
-	/**
-	 * Returns a filtered sequence.
-	 * Same effect as .transform(filter).
-	 * @param {IterableValueTransform<T, TResult>} filter
-	 * @return {Linq<TResult>}
-	 */
-	filter<TResult>(filter: IterableValueTransform<T, TResult>): LinqExtended<TResult>;
-
-	/**
-	 * Returns a filtered sequence.
-	 * @param {IterableFilter<T>} filters The filters to use.
-	 * @return {Linq<T>}
-	 */
-	filter(filter: IterableFilter<T>): LinqExtended<T>;
-
-	/**
-	 * Returns a filtered sequence.
-	 * @param {IterableFilter<T>} filters The filters to use.
-	 * @return {TLinq<T>}
-	 */
-	filter(filter: IterableFilter<T>): LinqExtended<T> {
-		return super.filter(filter);
-	}
-
-	/**
-	 * Returns a transformed sequence.
-	 * @param {IterableValueTransform<T, TResult>} transform The transform to use.
-	 * @return {LinqExtended<TResult>}
-	 */
-	transform<TResult>(transform: IterableValueTransform<T, TResult>): LinqExtended<TResult> {
-		return new LinqExtended(transform(this.source));
-	}
-
-	/**
-	 * Projects each element of a sequence into a new form.
-	 * @param {SelectorWithIndex<T, TResult>} selector
-	 * @return {LinqExtended<TResult>}
-	 */
-	select<TResult>(selector: SelectorWithIndex<T, TResult>): LinqExtended<TResult> {
-		return this.transform(select(selector));
-	}
-
-	/**
-	 * Projects each element of iterables as a flattened sequence of the selected.
-	 * @param {SelectorWithIndex<T, Iterable<TResult>>} selector
-	 * @return {LinqExtended<TResult>}
-	 */
-	selectMany<TResult>(selector: SelectorWithIndex<T, Iterable<TResult>>): LinqExtended<TResult> {
-		return this.transform(selectMany(selector));
-	}
-
-	/**
-	 * Groups entries together by selected key.
-	 * @param {SelectorWithIndex<T, TKey>} keySelector
-	 * @return {LinqExtended<Grouping<TKey, T>>}
-	 */
-	groupBy<TKey>(keySelector: SelectorWithIndex<T, TKey>): LinqExtended<LinqGrouping<TKey, T>> {
-		return this
-			.transform(groupBy(keySelector))
-			.select(g => new LinqGrouping<TKey, T>(g));
-	}
-
+class LinqExtended<T> extends LinqExtendedBase<T, LinqExtended<T>> {
+  constructor(source: Iterable<T>) {
+    super(source, linqExtended);
+  }
 }
 
-export class LinqGrouping<TKey, T>
-	extends LinqExtended<T>
-	implements Grouping<TKey, T>
-{
-	readonly key: TKey;
+export class LinqGrouping<TKey, T> extends LinqExtended<T> implements Grouping<TKey, T> {
+  readonly key: TKey;
 
-	constructor(grouping: GroupingResult<TKey, T>) {
-		super(grouping.elements);
-		this.key = grouping.key;
-	}
+  constructor(grouping: GroupingResult<TKey, T>) {
+    super(grouping.elements);
+    this.key = grouping.key;
+  }
 }
 
 /**
@@ -104,6 +209,6 @@ export class LinqGrouping<TKey, T>
  * @return {Linq<T>}
  */
 export default function linqExtended<T>(source: Iterable<T>): LinqExtended<T> {
-	if (source instanceof LinqExtended) return source;
-	return new LinqExtended<T>(source);
+  if (source instanceof LinqExtended) return source;
+  return new LinqExtended<T>(source);
 }
