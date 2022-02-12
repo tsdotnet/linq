e49cf8b9eb26921e0ab16ebcee2d0efbb4b37088
electricessence <5899455+electricessence@users.noreply.github.com>
2022-02-12 13:06:25 -0800

    Updated refrences

--- src/IterableTransform.d.ts
+++ src/IterableTransform.d.ts
@@ -8,6 +8,6 @@ export interface IterableTransform<T, TResult>
 	(e: Iterable<T>): TResult;
 }
 
-export type IterableValueTransform<TIn, TOut> = IterableTransform<TIn, Iterable<TOut>>
+export type IterableValueTransform<TIn, TOut> = IterableTransform<TIn, Iterable<TOut>>;
 
 export type IterableFilter<T> = IterableValueTransform<T, T>;
